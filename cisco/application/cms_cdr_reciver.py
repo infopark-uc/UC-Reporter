300b8aba008d4387442f234d8848a5b21b117382
AKozlov <artkozlov78@gmail.com>
2020-04-27 19:00:43 +0300

    Переделан модуль CMS CDR Reciver - 2-й раз

--- cisco/application/cms_cdr_reciver.py
+++ cisco/application/cms_cdr_reciver.py
@@ -12,14 +12,20 @@ def cdr_receiver():
         cdr_dict = json.loads(json.dumps(cdr)) #trasfrorm OrderedDict to Dict
         cms_ip = str(request.environ['REMOTE_ADDR']) #забираем IP
 
-        if type (cdr_dict['records']['record']) is list
+        if type (cdr_dict['records']['record']) is list:
+            print("CMS_RECEIVER: We get record list")
             record_list = cdr_dict['records']['record']
-        elif:
+        else:
+            print("CMS_RECEIVER: We get not record list")
             record_list = [cdr_dict['records']['record']]
 
+        print("CMS_RECEIVER: Number of records in list: " + record_list.count())
+
         for record_item in record_list:
+            print("CMS_RECEIVER: record_item")
+            pprint(record_item)
             if record_item['@type'] == 'callLegStart':  #проверяем что, это новый коллег
-                print("CMS_CDR: We get callLegStart")
+                print("CMS_RECEIVER: We get callLegStart")
                 callleg_id = str(record_item['callLeg']['@id'])  # забираем callleg ID
                 sipcall_id = str(record_item['callLeg']['sipCallId'])  # забираем sipcall_id
                 remoteAddress = str(record_item['callLeg']['remoteAddress'])  # забираем  remoteAddress
@@ -31,13 +37,13 @@ def cdr_receiver():
                 ### добавляем идентификаторы в базу
                 cms_sql_request(
                     "INSERT INTO cms_cdr_records SET date='" + timenow + "', startTime='" + callLegStartTime + "',cms_ip='" + cms_ip + "',callleg_id='" + callleg_id + "',sipcall_id='" + sipcall_id + "',localAddress='" + localAddress + "',remoteAddress='" + remoteAddress + "';")
-                print("CMS_CDR:     SIP ID: " + sipcall_id + " and " + callleg_id + " inserted to database")
+                print("CMS_RECEIVER:     SIP ID: " + sipcall_id + " and " + callleg_id + " inserted to database")
                 #собираем статистику по данному вызову
                 callleginfo(callleg_id,cms_ip)
-                print("CMS_CDR: requests to CMS " + cms_ip + " for "+ callleg_id + " is stop")
+                print("CMS_RECEIVER: requests to CMS " + cms_ip + " for "+ callleg_id + " is stop")
 
             if record_item['@type'] == 'callLegUpdate':
-                print("CMS_CDR: we get callLegUpdate")
+                print("CMS_RECEIVER: we get callLegUpdate")
                 callleg_id = str(record_item['callLeg']['@id'])  # забираем callleg ID для обновления базы данных
                 if "call" in record_item['callLeg']:
                     call_id = str(record_item['callLeg']['call'])  # забираем  reason
@@ -46,7 +52,7 @@ def cdr_receiver():
                         "UPDATE cms_cdr_records,cms_cdr_calls SET coSpace_name=cms_cdr_calls.name WHERE cms_cdr_calls.id='" + call_id + "';") # берем Имя спэйса из другой таблицы.
 
             if record_item['@type'] == 'callLegEnd':
-                print("CMS_CDR: we get callLegEnd")
+                print("CMS_RECEIVER: we get callLegEnd")
                 callleg_id = str(record_item['callLeg']['@id'])  # забираем callleg ID для обновления базы данных
                 durationSeconds = str(record_item['callLeg']['durationSeconds'])  # забираем  durationSeconds
                 reason = str(record_item['callLeg']['reason'])  # забираем  reason
@@ -78,14 +84,14 @@ def cdr_receiver():
 
                 #print("video codec TX: " + vcodectx)
                 ### обновляем информацию о вызове
-                print("CMS_CDR: insert CallLegEnd data to database")
+                print("CMS_RECEIVER: insert CallLegEnd data to database")
                 cms_sql_request(
                     "UPDATE cms_cdr_records SET txAudio_codec='" + acodectx + "',endTime='" + callLegEndTime + "',durationSeconds='" + durationSeconds + "',reason='" + reason + "',txVideo_codec='" + vcodectx + "',rxVideo_codec='" + vcodecrx + "',rxAudio_codec='" + acodecrx + "' WHERE callleg_id='" + callleg_id + "';")
-                print("CMS_CDR:  call detail updated from callLegEnd")
+                print("CMS_RECEIVER:  call detail updated from callLegEnd")
                 #pprint(cdr_dict)
 
             if record_item['@type'] == 'callStart':
-                print("CMS_CDR: we get callStart")
+                print("CMS_RECEIVER: we get callStart")
                 call_id = str(record_item['call']['@id'])
                 coSpace = str(record_item['call']['coSpace'])
                 name = str(record_item['call']['name'])
@@ -106,7 +112,7 @@ def cdr_receiver():
         return('', 204)
 
     except:
-        print('CMS_CDR: Parser failure!')
+        print('CMS_RECEIVER: Parser failure!')
         pprint(cdr_dict)
         return('', 204)
 
