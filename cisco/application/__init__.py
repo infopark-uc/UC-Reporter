85f9d494e01203518783a2276441ed9ecb31f65b
AVKovalev <yaazard@gmail.com>
2021-01-20 11:40:48 +0300

    добавлен конфиг для алхимии

--- cisco/application/__init__.py
+++ cisco/application/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask_login import LoginManager
 import logging, logging.handlers
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-
+#login
 login = LoginManager(app)
 login.login_view = 'login'
+#SQLAlchemy
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from application import routes
 
